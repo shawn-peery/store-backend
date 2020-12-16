dd3cf1982b7be1d8ee1d656931d117710d8c7450
Shawn Whitaker <shawn.w8465@gmail.com>
2020-12-15 17:13:07 -0700

    Can see

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const Item = require("../models/Item");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  Item.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("app", { items: items });
+    }
+  });
 });
 
 module.exports = router;
