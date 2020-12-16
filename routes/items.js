fb859d747eaeaf6121561bc6db9caceac9b8cd32
Shawn Whitaker <shawn.w8465@gmail.com>
2020-12-15 17:05:12 -0700

    Working with given image!

--- routes/items.js
+++ routes/items.js
@@ -13,68 +13,35 @@ const storage = multer.diskStorage({
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    cb(null, "NOOOOOOOOOO" + "-" + Date.now() + ".png");
+    cb(null, file.fieldname + "-" + Date.now());
   },
 });
 
 const upload = multer({ storage: storage });
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const items = await Item.find();
-//     res.status(200).send(items);
-//   } catch {
-//     res.status(400).send(err);
-//   }
-// });
+router.get("/", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).send(items);
+  } catch {
+    res.status(400).send(err);
+  }
+});
 
-// router.get("/:id", async (req, res) => {
-//   const itemId = request.params.id;
+router.get("/:id", async (req, res) => {
+  const itemId = request.params.id;
 
-//   try {
-//     const item = await Item.findById(itemId);
-//     res.status(200).send(item);
-//   } catch {
-//     res.status(400).send(err);
-//   }
-// });
+  try {
+    const item = await Item.findById(itemId);
+    res.status(200).send(item);
+  } catch {
+    res.status(400).send(err);
+  }
+});
 
 router.post("/", upload.single("ItemPhoto"), async (req, res) => {
-  console.log("req.body");
-  console.log(req.body);
-
-  console.log("Hello");
   try {
-    const filePath = path.join(
-      NODE_PATH + "/uploads/" + "ItemPhoto-1608073612479.png"
-    );
-
-    fs.readdir(path.join(NODE_PATH + "/uploads/"), (err, files) => {
-      console.log("Printing Files");
-      files.forEach((file) => {
-        console.log(file);
-      });
-    });
-
-    console.log(filePath);
-    console.log("path");
-
-    console.log("Exists:");
-    console.log(fs.existsSync(filePath));
-
-    // fs.mkdir(
-    //   path.join(NODE_PATH + "/uploads/" + "taco"),
-    //   { recursive: true },
-    //   (err) => {
-    //     if (err) throw err;
-
-    //     console.log("Succesfully created file.");
-    //     console.log("Exists now:");
-    //     console.log(fs.existsSync(path.join(NODE_PATH + "/uploads/" + "taco")));
-    //   }
-    // );
-
-    // console.log("access path");
+    const filePath = path.join(NODE_PATH + "/uploads/" + req.file.filename);
 
     const savedItem = {
       Name: req.body.Name,
@@ -101,53 +68,53 @@ router.post("/", upload.single("ItemPhoto"), async (req, res) => {
   }
 });
 
-// router.put("/:id", async (req, res) => {
-//   const itemId = request.params.id;
+router.put("/:id", async (req, res) => {
+  const itemId = request.params.id;
 
-//   const replacedItem = await {
-//     Name: req.body.name,
-//     Quantity: req.body.Quantity,
-//     MarketPrice: req.body.marketprice,
-//     SellerValue: req.body.sellervalue,
-//     AllianceValue: req.body.alliancevalue,
-//     ItemPhoto: {
-//       data: fs.readFileSync(
-//         path.join(NODE_PATH + "/uploads/" + req.file.filename)
-//       ),
-//       contentType: "image/png",
-//     },
-//   };
+  const replacedItem = await {
+    Name: req.body.name,
+    Quantity: req.body.Quantity,
+    MarketPrice: req.body.marketprice,
+    SellerValue: req.body.sellervalue,
+    AllianceValue: req.body.alliancevalue,
+    ItemPhoto: {
+      data: fs.readFileSync(
+        path.join(NODE_PATH + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
 
-//   try {
-//     const updatedItem = await Item.replaceOne(
-//       { _id: req.params.id },
-//       replacedItem
-//     );
-//     res.status(200).send(updatedItem);
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
-// });
+  try {
+    const updatedItem = await Item.replaceOne(
+      { _id: req.params.id },
+      replacedItem
+    );
+    res.status(200).send(updatedItem);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-// router.patch("/:id", async (req, res) => {
-//   try {
-//     const updatedItem = await Item.updateOne(
-//       { _id: req.params.id },
-//       { $set: req.body }
-//     );
-//     res.status(200).send(updatedItem);
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
-// });
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedItem = await Item.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).send(updatedItem);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const deletedItem = await Item.remove({ _id: req.params.id });
-//     res.status(200).send(deletedItem);
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
-// });
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedItem = await Item.remove({ _id: req.params.id });
+    res.status(200).send(deletedItem);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = router;
